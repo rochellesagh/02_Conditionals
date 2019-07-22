694eaf68c3cba0acf94a6bcd33df11192fd1342c
Apple <apple@app.local>
2019-07-22 15:39:07 -0400

    files

---
+++ Contents.swift
@@ -0,0 +1,27 @@
+import UIKit
+
+//var str = "Hello, playground"
+//Start with these variables:
+
+var a = 12
+var b = 65
+var c = 31
+var d = 98
+//Write code to find the average of these four numbers.
+//A-D Added Together
+print (a + b + c + d)
+//A-D Average
+print ((a + b + c + d) / 4)
+
+//Find the average yourself using paper or a calculator. Is your answer different than you found with Swift? Why?
+51.5
+//because it rounds to an integer
+//Say you have the operation a + b * c / d. What result do you get out from Swift? What other outputs can you get out by adding one or more pairs of parentheses to the equation?
+print ((a + b) * (c/d))
+
+
+5<3 // false
+12>7 // true
+6 !=8 // true
+7 == "7" // false
+
